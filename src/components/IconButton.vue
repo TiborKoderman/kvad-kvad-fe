c132a374348c52206e837685f1790a451f93d2bd
Tibor Koderman <tibor.koderman@inea.si>
2025-03-26 08:37:10 +0100

    Add IconButton component and integrate it into DashboardView for edit and delete actions

---
+++ src/components/IconButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <i :class="`bi bi-${props.icon}`"></i>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'var(--bs-dark)',
+  },
+  hcolor: {
+    type: String,
+    default: 'var(--bs-primary)',
+  },
+})
+</script>
+
+<style scoped>
+i {
+  color: v-bind(color);
+}
+
+i:hover {
+  cursor: pointer;
+  color: v-bind(hcolor);
+}
+</style>
