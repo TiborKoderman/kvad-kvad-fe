bb7d0f6cf1d67afa7ad7411f71354040d6930a0c
Tibor Koderman <tibor.koderman@gmail.com>
2025-03-27 23:09:32 +0100

    Refactor TopBar component for improved button styling and layout consistency

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -1,32 +1,33 @@
 <!-- TopBar.vue -->
 <template>
   <div class="topbar navbar-expand-lg border-bottom" :class="topbarClass">
-      <button
-        @click="menu.toggleCollapsed"
-        class="btn btn-outline-light text-decoration-none me-2"
-        aria-label="Expand sidebar"
-      >
-        <i class="bi bi-layout-sidebar text-black"></i>
-      </button>
-      <!-- Additional TopBar content -->
-       <button
-        @click="menu.toggleEditMode"
-        :class="{'btn-primary': menu.editMode}"
-        class="btn btn-outline-light text-decoration-none me-2"
-        aria-label="Edit mode"
-       >
-       <i class="bi bi-pencil "
-       :class="menu.editMode ? 'text-light' : 'text-dark'"
-       ></i>
-       </button>
-       
-      <UserIcon class="ms-auto" />
+    <button
+      @click="menu.toggleCollapsed"
+      class="btn btn-outline-light text-decoration-none me-2"
+      aria-label="Expand sidebar"
+    >
+      <i class="bi bi-layout-sidebar text-black"></i>
+    </button>
+    <!-- Additional TopBar content -->
+    <button
+      @click="menu.toggleEditMode"
+      :class="menu.editMode ? 'btn-primary' : 'btn-outline-light'"
+      class="btn text-decoration-none me-2"
+      aria-label="Edit mode"
+    >
+      <i
+        class="bi bi-pencil"
+        :class="menu.editMode ? 'text-light' : 'text-dark'"
+      ></i>
+    </button>
+
+    <UserIcon class="ms-auto" />
   </div>
 </template>
 
 <script setup lang="ts">
 import UserIcon from '@/components/UserIcon.vue'
-import useMenuStore from '@/stores/menu';
+import useMenuStore from '@/stores/menu'
 
 const menu = useMenuStore()
 
@@ -40,7 +41,6 @@ defineProps({
     default: false,
   },
 })
-
 </script>
 
 <style scoped>
