b74eb27f40d0c0e5d733f6bc63c5c9e6b77365fe
Tibor Koderman <tibor.koderman@inea.si>
2025-03-28 15:09:13 +0100

    Refactor PowerCalculation component to improve chart rendering; add sectorRenderer for angle visualization and enhance tooltip functionality

--- src/components/Dashboard/PowerCalculation.vue
+++ src/components/Dashboard/PowerCalculation.vue
@@ -11,7 +11,7 @@
 
 <script setup lang="ts">
 import VChart from 'vue-echarts'
-import { computed, PropType, ref, shallowRef } from 'vue'
+import { computed, PropType, shallowRef } from 'vue'
 import * as echarts from 'echarts/core'
 import { LineChart, CustomChart, ScatterChart } from 'echarts/charts'
 import {
@@ -47,8 +47,8 @@ const props = defineProps({
     required: true,
     default: () => [
       { V: 230, I: 10, cosPhi: 0.8 },
-      { V: 230, I: 11, cosPhi: 0.6 },
-      { V: 235, I: 13, cosPhi: 0.9 },
+      { V: 230, I: 5, cosPhi: 0.6 },
+      { V: 235, I: 13, cosPhi: 0.99 },
     ],
   },
   radians: {
@@ -108,11 +108,18 @@ const chartOption = shallowRef({
     { id: 'L2', center: ['50%', '50%'] },
     { id: 'L3', center: ['50%', '50%'] },
   ],
+  grid: {
+    top: 20,
+    bottom: 20,
+    left: 20,
+    right: 20,
+    containLabel: true,
+  },
   tooltip: {
     trigger: 'item',
-    // axisPointer: {
-    //   type: 'cross',
-    // },
+    axisPointer: {
+      type: 'cross',
+    },
   },
   angleAxis: [
     {
@@ -171,7 +178,8 @@ const chartOption = shallowRef({
       coordinateSystem: 'polar',
       polarIndex: 0,
       color: 'blue',
-      renderItem: (params, api) => vectorRenderer(params, api, `${powers.value[0].V.toFixed(1)} V`),
+      renderItem: (params, api) =>
+        vectorRenderer(params, api, `${powers.value[0].V.toFixed(1)} V`),
       data: [[0, 0, powers.value[0].VNormalized(), 0]],
       tooltip: {
         show: true,
@@ -183,8 +191,11 @@ const chartOption = shallowRef({
       coordinateSystem: 'polar',
       polarIndex: 0,
       color: 'blue',
-      renderItem: (params, api) => vectorRenderer(params, api, `${powers.value[0].I.toFixed(1)} A`),
-      data: [[0, 0, powers.value[0].INormalized(), powers.value[0].phiDegrees()]],
+      renderItem: (params, api) =>
+        vectorRenderer(params, api, `${powers.value[0].I.toFixed(1)} A`),
+      data: [
+        [0, 0, powers.value[0].INormalized(), powers.value[0].phiDegrees()],
+      ],
       tooltip: {
         show: true,
         formatter: `L1: ${powers.value[0].I.toFixed(1)} A`,
@@ -193,9 +204,24 @@ const chartOption = shallowRef({
     {
       type: 'custom',
       coordinateSystem: 'polar',
+      polarIndex: 0,
+      color: 'rgba(0, 0, 255, 0.2)', // Semi-transparent blue for the sector
+      renderItem: sectorRenderer,
+      data: [
+      [0, 0, powers.value[0].INormalized(), powers.value[0].phiDegrees()],
+      ],
+      tooltip: {
+      show: true,
+      formatter: `Angle: ${powers.value[0].phiDegrees().toFixed(1)}°`,
+      },
+    },
+    {
+      type: 'custom',
+      coordinateSystem: 'polar',
       polarIndex: 1,
       color: 'green',
-      renderItem: (params, api) => vectorRenderer(params, api, `${powers.value[1].V.toFixed(1)} V`),
+      renderItem: (params, api) =>
+        vectorRenderer(params, api, `${powers.value[1].V.toFixed(1)} V`),
       data: [[0, 0, powers.value[1].VNormalized(), 0]],
       tooltip: {
         show: true,
@@ -207,11 +233,28 @@ const chartOption = shallowRef({
       coordinateSystem: 'polar',
       polarIndex: 1,
       color: 'green',
-      renderItem: (params, api) => vectorRenderer(params, api, `${powers.value[1].I.toFixed(1)} A`),
-      data: [[0, 0, powers.value[1].INormalized(), powers.value[1].phiDegrees()]],
+      renderItem: (params, api) =>
+        vectorRenderer(params, api, `${powers.value[1].I.toFixed(1)} A`),
+      data: [
+        [0, 0, powers.value[1].INormalized(), powers.value[1].phiDegrees()],
+      ],
+      tooltip: {
+        show: true,
+        formatter: `L1: ${powers.value[0].I.toFixed(1)} A`,
+      },
+    },
+    {
+      type: 'custom',
+      coordinateSystem: 'polar',
+      polarIndex: 1,
+      color: 'rgba(0,255, 0, 0.2)', // Semi-transparent blue for the sector
+      renderItem: sectorRenderer,
+      data: [
+      [0, 0, powers.value[1].INormalized(), powers.value[1].phiDegrees()],
+      ],
       tooltip: {
       show: true,
-      formatter: `L1: ${powers.value[0].I.toFixed(1)} A`,
+      formatter: `Angle: ${powers.value[1].phiDegrees().toFixed(1)}°`,
       },
     },
     {
@@ -219,7 +262,8 @@ const chartOption = shallowRef({
       coordinateSystem: 'polar',
       polarIndex: 2,
       color: 'red',
-      renderItem: (params, api) => vectorRenderer(params, api, `${powers.value[2].V.toFixed(1)} V`),
+      renderItem: (params, api) =>
+        vectorRenderer(params, api, `${powers.value[2].V.toFixed(1)} V`),
       data: [[0, 0, powers.value[2].VNormalized(), 0]],
       tooltip: {
         show: true,
@@ -231,11 +275,28 @@ const chartOption = shallowRef({
       coordinateSystem: 'polar',
       polarIndex: 2,
       color: 'red',
-      renderItem: (params, api) => vectorRenderer(params, api, `${powers.value[2].I.toFixed(1)} A`),
-      data: [[0, 0, powers.value[2].INormalized(), powers.value[2].phiDegrees()]],
+      renderItem: (params, api) =>
+        vectorRenderer(params, api, `${powers.value[2].I.toFixed(1)} A`),
+      data: [
+        [0, 0, powers.value[2].INormalized(), powers.value[2].phiDegrees()],
+      ],
       tooltip: {
         show: true,
         formatter: `L1: ${powers.value[2].I.toFixed(1)} A`,
+      },
+    },
+    {
+      type: 'custom',
+      coordinateSystem: 'polar',
+      polarIndex: 2,
+      color: 'rgba(255,0, 0, 0.2)', // Semi-transparent blue for the sector
+      renderItem: sectorRenderer,
+      data: [
+      [0, 0, powers.value[2].INormalized(), powers.value[2].phiDegrees()],
+      ],
+      tooltip: {
+      show: true,
+      formatter: `Angle: ${powers.value[2].phiDegrees().toFixed(1)}°`,
       },
     },
   ],
@@ -246,11 +307,14 @@ function vectorRenderer(params, api, text) {
   const radius = api.value(2)
   const angle = api.value(3)
 
-  
   const end = api.coord([radius, angle])
 
   console.log('origin', origin)
   console.log('end', end)
+
+  const textOffset = 0.15 // Offset for the text position
+
+  const textCoord = api.coord([radius + textOffset, angle])
 
   const color = api.visual('color')
 
@@ -270,13 +334,12 @@ function vectorRenderer(params, api, text) {
         },
       },
 
-
       {
         type: 'rect',
         shape: {
-          x: end[0] + (end[0] - origin[0]) * 0.2 - 20, // Adjust position for textbox
-          y: end[1] + (end[1] - origin[1]) * 0.2 - 10, // Adjust position for textbox
-          width: 40, // Width of the textbox
+          x: textCoord[0] - 25, // Move text slightly outward along the vector
+          y: textCoord[1] - 10, // Move text slightly outward along the vector
+          width: 50, // Width of the textbox
           height: 20, // Height of the textbox
         },
         style: {
@@ -289,8 +352,8 @@ function vectorRenderer(params, api, text) {
         type: 'text',
         style: {
           text: text, // Display the radius value
-          x: end[0] + (end[0] - origin[0]) * 0.2, // Move text slightly outward along the vector
-          y: end[1] + (end[1] - origin[1]) * 0.2, // Move text slightly outward along the vector
+          x: textCoord[0], // Move text slightly outward along the vector
+          y: textCoord[1], // Move text slightly outward along the vector
           fill: color,
           font: '12px sans-serif',
           align: 'center',
@@ -308,6 +371,70 @@ function vectorRenderer(params, api, text) {
         },
         style: {
           fill: color,
+        },
+      },
+    ],
+  }
+}
+
+function sectorRenderer(params,api: any, text: string | undefined) {
+  const origin = api.coord([api.value(0), api.value(1)])
+  const radius = api.value(2)
+  const angle = api.value(3) // Make the angle negative for counterclockwise
+  const startAngle = api.value(1)
+
+  const end = api.coord([api.value(2), angle])
+
+  if (!text) {
+    text = `${api.value(3).toFixed(1)}°`
+  }
+
+  const textPosition = api.coord([api.value(0) + 0.35, angle / 2])
+
+  return {
+    type: 'group',
+    children: [
+      {
+        type: 'sector',
+        shape: {
+          cx: origin[0],
+          cy: origin[1],
+          r: radius,
+          r0: 0,
+          startAngle: (startAngle * Math.PI) / 180,
+          endAngle: (angle * Math.PI) / 180,
+          clockwise: true,
+        },
+        style: {
+          fill: api.visual('color'),
+          opacity: 0.3,
+        },
+      },
+      {
+        type: 'rect',
+        shape: {
+          x: textPosition[0] - 25, // Adjust position for the textbox
+          y: textPosition[1] - 10,
+          width: 50,
+          height: 20,
+        },
+        style: {
+          fill: 'rgba(255, 255, 255, 0.8)', // Background color with transparency
+            stroke: `rgba(${echarts.color.parse(api.visual('color')).slice(0, 3).join(',')}, 1)`, // Border color
+          lineWidth: 1,
+        },
+      },
+      {
+        type: 'text',
+        style: {
+          text: text,
+          x: textPosition[0],
+          y: textPosition[1],
+          fill: `rgba(${echarts.color.parse(api.visual('color')).slice(0, 3).join(',')}, 1)`,
+          font: '12px sans-serif',
+          fontWeight: 'bold',
+          align: 'center',
+          verticalAlign: 'middle',
         },
       },
     ],
